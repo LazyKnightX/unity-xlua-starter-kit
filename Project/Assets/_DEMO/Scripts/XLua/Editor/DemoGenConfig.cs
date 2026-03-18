c00d81b8f92431a86be480978e61a569f8aed52f
LazyKnight <i@lazyknight.com>
2026-03-18 20:45:03 +0800

    chore: 配置 xLua 生成代码配置与 git 忽略规则
    - 更新 gitignore 忽略 Assets/XLua/Gen 目录
    - 清理 DemoGenConfig (XLua Generate Code配置)

--- Project/Assets/_DEMO/Scripts/XLua/Editor/DemoGenConfig.cs
+++ Project/Assets/_DEMO/Scripts/XLua/Editor/DemoGenConfig.cs
@@ -11,52 +11,53 @@ public static class DemoGenConfig
     //lua中要使用到C#库的配置，比如C#标准库，或者Unity API，第三方库等。
     [LuaCallCSharp]
     public static List<Type> LuaCallCSharp = new List<Type>() {
-                typeof(System.Object),
-                typeof(UnityEngine.Object),
-                typeof(Vector2),
-                typeof(Vector3),
-                typeof(Vector4),
-                typeof(Quaternion),
-                typeof(Color),
-                typeof(Ray),
-                typeof(Bounds),
-                typeof(Ray2D),
-                typeof(Time),
-                typeof(GameObject),
-                typeof(Component),
-                typeof(Behaviour),
-                typeof(Transform),
-                typeof(Resources),
-                typeof(TextAsset),
-                typeof(Keyframe),
-                typeof(AnimationCurve),
-                typeof(AnimationClip),
-                typeof(MonoBehaviour),
-                typeof(ParticleSystem),
-                typeof(SkinnedMeshRenderer),
-                typeof(Renderer),
-                typeof(WWW),
-                typeof(Light),
-                typeof(Mathf),
-                typeof(System.Collections.Generic.List<int>),
-                typeof(Action<string>),
-                typeof(UnityEngine.Debug)
+                // typeof(System.Object),
+                // typeof(UnityEngine.Object),
+                // typeof(Vector2),
+                // typeof(Vector3),
+                // typeof(Vector4),
+                // typeof(Quaternion),
+                // typeof(Color),
+                // typeof(Ray),
+                // typeof(Bounds),
+                // typeof(Ray2D),
+                // typeof(Time),
+                // typeof(GameObject),
+                // typeof(Component),
+                // typeof(Behaviour),
+                // typeof(Transform),
+                // typeof(Resources),
+                // typeof(TextAsset),
+                // typeof(Keyframe),
+                // typeof(AnimationCurve),
+                // typeof(AnimationClip),
+                // typeof(MonoBehaviour),
+                // typeof(ParticleSystem),
+                // typeof(SkinnedMeshRenderer),
+                // typeof(Renderer),
+                // typeof(WWW),
+                // typeof(Light),
+                // typeof(Mathf),
+                // typeof(System.Collections.Generic.List<int>),
+                // typeof(Action<string>),
+                // typeof(UnityEngine.Debug)
             };
 
     //C#静态调用Lua的配置（包括事件的原型），仅可以配delegate，interface
     [CSharpCallLua]
     public static List<Type> CSharpCallLua = new List<Type>() {
-                typeof(Action),
-                typeof(Func<double, double, double>),
-                typeof(Action<string>),
-                typeof(Action<double>),
-                typeof(UnityEngine.Events.UnityAction),
-                typeof(System.Collections.IEnumerator)
+                // typeof(Action),
+                // typeof(Func<double, double, double>),
+                // typeof(Action<string>),
+                // typeof(Action<double>),
+                // typeof(UnityEngine.Events.UnityAction),
+                // typeof(System.Collections.IEnumerator)
             };
 
     //黑名单
     [BlackList]
     public static List<List<string>> BlackList = new List<List<string>>()  {
+        
                 new List<string>(){"System.Xml.XmlNodeList", "ItemOf"},
                 new List<string>(){"UnityEngine.WWW", "movie"},
     #if UNITY_WEBGL
